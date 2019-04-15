e3097ab134335bd57f643dfb8a6a353658e0006b
Chuang Ke <k1345265138@gmail.com>
2019-04-15 15:56:57 -0500

    Update fill_servant.py

--- backend/database_scripts/fill_servant.py
+++ backend/database_scripts/fill_servant.py
@@ -1,14 +1,10 @@
 import pymongo
-import servant_list from servant_src
+import servant_list from "../backend/database_scripts/servant_src"
 
 client = pymongo.MongoClient("mongodb+srv://chuangk2:Tea890122@caldeadb-buuxb.mongodb.net/test?retryWrites=true")
 
 db = client["chaldeadb"]
 
 col = db["servants"]
-
-servant_list = [
-    { "_id": 1, "name": "Mashu Kyrielight"}
-]
 
 col.insert_many(servant_list);
