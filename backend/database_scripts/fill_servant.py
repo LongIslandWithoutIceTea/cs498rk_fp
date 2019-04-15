640b836d7c2100636ada32fb1b073dcf89cda568
Ke, Chuang <chuangk2@illinois.edu>
2019-04-15 16:41:10 -0500

    Revert "whatever"
    This reverts commit 8b3086cddfd9b66becf49d6b1b1ed4d50a26f6fb.

--- backend/database_scripts/fill_servant.py
+++ backend/database_scripts/fill_servant.py
@@ -1,11 +1,10 @@
 import pymongo
-#from servant_src import servant_list
+from servant_src import servant_list
 
 client = pymongo.MongoClient("mongodb+srv://chuangk2:Tea890122@caldeadb-buuxb.mongodb.net/test?retryWrites=true")
 
 db = client["chaldeadb"]
 
 col = db["servants"]
-servant_list = [{"name": "C\xc3\xba Chulainn (Caster)", "star": 3, "_id": 38}]
 print(servant_list)
 col.insert_many(servant_list)
