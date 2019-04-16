284b2408e0fa4b33beb3dbf7ebeb4d579e184a99
Ke, Chuang <chuangk2@illinois.edu>
2019-04-15 23:25:54 -0500

    fill_servant not finished

--- backend/database_scripts/fill_servant.py
+++ backend/database_scripts/fill_servant.py
@@ -1,10 +1,23 @@
 import pymongo
 from servant_src import servant_list
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+base_url = "https://fategrandorder.fandom.com/wiki/"
 
 client = pymongo.MongoClient("mongodb+srv://chuangk2:Tea890122@caldeadb-buuxb.mongodb.net/test?retryWrites=true")
-
 db = client["chaldeadb"]
-
 col = db["servants"]
-print(servant_list)
-col.insert_many(servant_list)
+response = requests.get(base_url + servant_list[0]["name"])
+soup = BeautifulSoup(response.text, 'lxml')
+info = soup.find("div", class_="ServantInfoMain")
+stats = info.find("div", class_="ServantInfoStatsWrapper").find_all("table")
+
+print(stats[0])
+
+# for svt in servants:
+#     response = requests.get(base_url + svt["name"])
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     print(soup.title)
+#col.insert_many(servant_list)
