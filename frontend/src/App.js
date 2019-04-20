e86c8d1ab383c665896cb46c5b0cb8d6b46a5cbb
Quincy0v0 <30424046+Quincy0v0@users.noreply.github.com>
2019-04-20 18:53:45 -0500

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import IndexPage from './IndexPage/IndexPage'
+import { HashRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <IndexPage/>
+          <Router>
+              <div>
+                  <Route path={"/"} exact component={IndexPage} />
+                  <Route path={"/home"} component={Home} />
+              </div>
+          </Router>
       </div>
     );
   }
