f6cf542f145ea686bd4c4b23fa72ffbaddd04d0a
Ke, Chuang <chuangk2@illinois.edu>
2019-04-30 05:23:04 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cs498rk_fp/precache-manifest.3c69c0ab1199218b0472263a5f6e296c.js"
+  "/cs498rk_fp/precache-manifest.3dbc02ddc61ac892877f67c5d68b8c93.js"
 );
 
 workbox.clientsClaim();
