65dcee83368480a526bab413dd3eae3f05afc4ca
Quincy0v0 <30424046+Quincy0v0@users.noreply.github.com>
2019-04-30 05:28:37 -0500

    done!

--- frontend/src/User/ManagePost.js
+++ frontend/src/User/ManagePost.js
@@ -13,6 +13,7 @@ class ManagePost extends Component {
         post_list: null
     }
     this.build = this.build.bind(this);
+    this.deletePost = this.deletePost.bind(this);
   }
 
   componentDidMount() {
@@ -28,12 +29,34 @@ class ManagePost extends Component {
           })
   }
 
+  async deletePost(e, idx){
+      let post = this.state.post_list[idx]
+      e.preventDefault();
+      await axios.delete(server + "/posts/"+ post._id).
+      then((response)=>{
+          if (response.data.data && response.data.data._id === post._id){
+
+          }else{
+              console.log("different data" + response.data.data);
+          }
+      })
+          .catch((error) => {
+              console.log(error);
+          });
+      await axios.get(server + "/posts?where={\"user_post\":\""+ this.props.username+"\"}")
+          .then((response)=>{
+              let new_list = response.data.data;
+              console.log(response.data.data);
+              this.setState({post_list:new_list});
+          })
+  }
+
   render() {
       const content = this.state.post_list && this.state.post_list.map((post, idx) => {
           return (
               <Card key={idx} fluid color='violet'>
                   <Card.Content>
-                      <Card.Header content='Jake Smith' />
+                      <Card.Header content={post.ship_name} />
                       <Card.Meta content={post.date_created} />
                       <Card.Description>
                           <Grid>
@@ -41,7 +64,7 @@ class ManagePost extends Component {
                                   {post.content}
                               </Grid.Column>
                               <Grid.Column floated='right'>
-                                  <a>Delete</a>
+                                  <a onClick={(e) => this.deletePost(e, idx)}>Delete</a>
                               </Grid.Column>
                           </Grid>
                       </Card.Description>
